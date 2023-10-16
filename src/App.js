eabad6b85bf32735184b7bad3716c68bb6562fcd
tothbalazsipari <animefan199914@gmail.com>
2023-10-16 08:50:54 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Feltolt from "./components/feltolt"
+import { useState, useEffect, useCallback } from "react";
 
 function App() {
+  const [datas, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [newData, setNewData] = useState("");
+
+  function saveData(ujAdatok) {
+    fetch(
+      "https://react-test-e956d-default-rtdb.europe-west1.firebasedatabase.app/nyaral.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ujAdatok),
+        headers: { "Content-Type": "application/json" }
+      }
+    );
+    setData((prevData) => {
+      return [ujAdatok, ...prevData];
+    });
+  }
+
+  const megye = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://react-test-e956d-default-rtdb.europe-west1.firebasedatabase.app/nyaral.json"
+      );
+      if (!response.ok) {
+        throw new Error("Valami baj van");
+      }
+      const dataList = await response.json();
+      console.log(dataList);
+      const list = [];
+      for (const key in dataList) {
+        const aData = {
+          id: key,
+          ...dataList[key]
+        };
+        list.push(aData);
+      }
+      setData(list);
+      console.log(datas);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      alert(error.message);
+    }
+  }, []);
+  useEffect(() => {
+    setTimeout(() => megye(), 0);
+  }, [megye, newData]);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Feltolt/>
     </div>
   );
 }
---
+++ src/components/feltolt.js
@@ -0,0 +1,66 @@
+import { useRef } from "react";
+
+export default function addNewDataForm(props) {
+  const tantargy = useRef();
+  const datum = useRef();
+  const temakor = useRef();
+  const hatarido = useRef();
+  const milyen = useRef();
+  const leiras = useRef();
+
+  function FormHandler(e) {
+    e.preventDefault();
+  }
+
+  function submitHandler() {
+    const actualData = {
+      id: Math.random(),
+      tantargy: tantargy.current.value,
+      datum: datum.current.value,
+      temakor: temakor.current.value,
+      hatarido: hatarido.current.value,
+      milyen: milyen.current.value,
+      leiras: leiras.current.value
+    };
+    props.OnSaveHandler(actualData);
+  }
+
+  return (
+    <div>
+      <form onSubmit={FormHandler}>
+        <label for="tantargy">Tantárgy</label>
+        <label for = "datum" >Feladás dátuma</label>
+        <br/>
+        <select name="tantargy" id="tantargy" ref={tantargy}>
+          <option value="Matematika">Matematika</option>
+          <option value="Idegen nyelv">Idegen nyelv</option>
+          <option value="Magyar nyelvtan és Irodalom">Magyar nyelvtan és Irodalom</option>
+          <option value="Történelem">Történelem</option>
+          <option value="Természettudományok">Természettudományok</option>
+          <option value="Testnevelés">Testnevelés</option>
+          <option value="Szakmai">Szakmai</option>
+        </select>
+
+        <input type="date" ref={datum} />
+
+        <br/>
+        <label for = "temakor" >Témakör</label>
+        <label for = "hatarido" >Határidő</label>
+        <br/>
+
+        <input type="text" ref={temakor} />
+        <input type="text" ref={hatarido} />
+
+        <input type="radio" id="irasbeli" name="irasbeli" value="Írásbeli" ref={milyen}/>
+        <label for="html">Írásbeli</label><br/>
+        <input type="radio" id="szobeli" name="irasbeli" value="Szóbeli" ref={milyen}/>
+        <label for="Szóbeli">Szóbeli</label><br/>
+        
+        <label for = "leiras" >Leírás</label>
+        <input type="text" ref={leiras} />
+        <br/>
+        <input type="submit" value="Felvesz" onClick={submitHandler} />
+      </form>
+    </div>
+  );
+}
